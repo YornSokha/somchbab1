4b73a461943cf8951f9cbdc211cb6b4e40924aad
LunSim <lunsim168@gmail.com>
2019-07-13 01:01:50 +0700

    Update dashboard Academic

--- Controller/src/main/java/com/hrd/somchbab/controller/DashboardController.java
+++ Controller/src/main/java/com/hrd/somchbab/controller/DashboardController.java
@@ -1,7 +1,12 @@
 package com.hrd.somchbab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    @GetMapping("/dashboard")
+    public String dashboard(){
+    return "/dashboard/dashboard";
+    }
 }
